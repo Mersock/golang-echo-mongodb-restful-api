b9941dff2f969c26a3c6923c20b99d52e5d3cdb2
Knight <knzphumthawan@gmail.com>
2021-05-05 16:58:29 +0700

    add validator

--- handlers/products.go
+++ handlers/products.go
@@ -6,9 +6,18 @@ import (
 	"net/http"
 
 	"github.com/Mersock/golang-echo-mongodb-restful-api/dbiface"
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+var (
+	v = validator.New()
+)
+
+type ProductValidator struct {
+	validator *validator.Validate
+}
 
 type Product struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
@@ -23,6 +32,10 @@ type Product struct {
 
 type ProductHandlers struct {
 	Col dbiface.CollectionAPI
+}
+
+func (p *ProductValidator) Validate(i interface{}) error {
+	return p.validator.Struct(i)
 }
 
 func insertProducts(ctx context.Context, products []Product, collection dbiface.CollectionAPI) ([]interface{}, error) {
@@ -41,10 +54,19 @@ func insertProducts(ctx context.Context, products []Product, collection dbiface.
 
 func (h *ProductHandlers) CreateProducts(c echo.Context) error {
 	var products []Product
+	c.Echo().Validator = &ProductValidator{validator: v}
 
 	if err := c.Bind(&products); err != nil {
 		log.Printf("Unable to bind: %v", err)
 		return err
+	}
+
+	for _, product := range products {
+		if err := c.Validate(product); err != nil {
+			log.Printf("Unable to validate the product %v %+v", product, err)
+			return err
+		}
+
 	}
 
 	IDs, err := insertProducts(context.Background(), products, h.Col)
