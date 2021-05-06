e0fd2d6d6b66c3b82945cd893b3245652b3118c9
Knight <knzphumthawan@gmail.com>
2021-05-06 11:14:25 +0700

    change main state

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 
 	"github.com/Mersock/golang-echo-mongodb-restful-api/config"
+	"github.com/Mersock/golang-echo-mongodb-restful-api/db"
 	"github.com/Mersock/golang-echo-mongodb-restful-api/handlers"
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
-	c   *mongo.Client
-	db  *mongo.Database
-	col *mongo.Collection
 	cfg config.Properties
 )
 
@@ -25,22 +20,13 @@ func init() {
 	if err := cleanenv.ReadEnv(&cfg); err != nil {
 		log.Fatalf("Configuration cannot be read : %v", err)
 	}
-
-	connURI := fmt.Sprintf("mongodb://%s:%s@mongo/?authSource=admin", cfg.DBUser, cfg.DBPass)
-
-	c, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connURI))
-
-	if err != nil {
-		log.Fatalf("Unable to conntect to database : %s", err)
-	}
-
-	db = c.Database(cfg.DBName)
-	col = db.Collection(cfg.CollectionName)
 }
 
 func main() {
 	e := echo.New()
+	col := db.New()
 	h := handlers.ProductHandlers{Col: col}
+
 	e.Pre(middleware.RemoveTrailingSlash())
 
 	e.POST("/products", h.CreateProducts)
