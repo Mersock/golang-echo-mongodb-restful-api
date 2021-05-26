a27917f522c9df6ecad7402b58a6ca02e494432a
Knight <knzphumthawan@gmail.com>
2021-05-26 18:30:30 +0700

    add jwt config

--- config/config.go
+++ config/config.go
@@ -12,4 +12,5 @@ type Properties struct {
 	DBPass            string `env:"DB_PASS" env-default:"123456"`
 	ProductCollection string `env:"PRODUCTS_COL_NAME" env-default:"products"`
 	UserCollection    string `env:"USERS_COL_NAME" env-default:"users"`
+	JwtTokenSecret    string `env:"JWT_TOKEN_SECRET" env-default:"27rmzGBbdlNCpHltxogK69DSqjdhPttQSBkm_F3SvJG4XTes6Kjts5LhENBZDVhtSRda_FJKwcRVR0V4iSS30t2cDvD5ZF_ZIK4aLjOOP8jPCZjmamhidAhHP8MIZ-0MtRlDf4HVqRDWTRXoIoXKDBEC2VntRC7nhKL1OqfplZk"`
 }
