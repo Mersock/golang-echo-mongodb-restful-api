5f6f473c26e84f3bd3ae10a2b821e9cd31173e99
Knight <knzphumthawan@gmail.com>
2021-05-28 23:19:52 +0700

    add auth user test

--- handlers/users_test.go
+++ handlers/users_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -11,7 +12,7 @@ import (
 )
 
 func TestUsers(t *testing.T) {
-	t.Run("Test create user invalid data", func(t *testing.T) {
+	t.Run("Test create User Invalid Data", func(t *testing.T) {
 		body := `{
 			"username":"knz@email.com,
 			"password":"1234"
@@ -27,8 +28,7 @@ func TestUsers(t *testing.T) {
 		assert.Equal(t, http.StatusBadRequest, res.Code)
 	})
 
-	t.Run("Test Create user", func(t *testing.T) {
-		// var user User
+	t.Run("Test Create User", func(t *testing.T) {
 		body := `
 			{
 				"username":"knz@email.com",
@@ -46,14 +46,9 @@ func TestUsers(t *testing.T) {
 		assert.Equal(t, http.StatusCreated, res.Code)
 		token := res.Header().Get("X-Auth-token")
 		assert.NotEmpty(t, token)
-		// err = json.Unmarshal(res.Body.Bytes(), &user)
-		// t.Logf("user: %v", res.Body.String())
-		// assert.Nil(t, err)
-		// assert.Equal(t, "knz@email.com", user.Email)
-		// assert.Empty(t, user.Password)
 	})
 
-	t.Run("Test Create duplicate", func(t *testing.T) {
+	t.Run("Test Create Duplicate", func(t *testing.T) {
 		body := `
 			{
 				"username":"knz@email.com",
@@ -69,5 +64,31 @@ func TestUsers(t *testing.T) {
 		err := uh.CreateUser(c)
 		assert.Nil(t, err)
 		assert.Equal(t, http.StatusBadRequest, res.Code)
+	})
+
+	t.Run("Test Auth User", func(t *testing.T) {
+		var user User
+		body := `
+			{
+				"username":"knz@email.com",
+				"password":"12345678"
+			}	
+		`
+		req := httptest.NewRequest(http.MethodPost, "/auth", strings.NewReader((body)))
+		res := httptest.NewRecorder()
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		e := echo.New()
+		c := e.NewContext(req, res)
+		uh.Col = usersCol
+		err := uh.AuthUser(c)
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusOK, res.Code)
+		token := res.Header().Get("X-Auth-token")
+		assert.NotEmpty(t, token)
+		err = json.Unmarshal(res.Body.Bytes(), &user)
+		t.Logf("user: %v", res.Body.String())
+		assert.Nil(t, err)
+		assert.Equal(t, "knz@email.com", user.Email)
+		assert.Empty(t, user.Password)
 	})
 }
